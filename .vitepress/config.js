ecfede215a8c9d9d157ffeda49bb5504761d42d1
MikeMizanin <126438504+MikeMizanin@users.noreply.github.com>
2026-05-30 00:53:12 -0300

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,17 +2,31 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: 'TaskBarHero Wiki ES',
-  description: 'La Wiki en Español de TaskBarHero',
+  description: 'Wiki en Español de TaskBarHero',
+
+  lang: 'es-ES',
+
+  lastUpdated: true,
 
   themeConfig: {
+    logo: '/logo.png',
+
     nav: [
       { text: 'Inicio', link: '/' },
-      { text: 'Héroes', link: '/heroes/knight' }
+      { text: 'Héroes', link: '/heroes/knight' },
+      { text: 'Objetos', link: '/items/' },
+      { text: 'Guías', link: '/guides/' },
+      { text: 'Tier List', link: '/tier-list/' }
     ],
+
+    search: {
+      provider: 'local'
+    },
 
     sidebar: [
       {
         text: 'Héroes',
+        collapsed: false,
         items: [
           { text: 'Knight', link: '/heroes/knight' },
           { text: 'Ranger', link: '/heroes/ranger' },
@@ -21,7 +35,34 @@ export default defineConfig({
           { text: 'Hunter', link: '/heroes/hunter' },
           { text: 'Slayer', link: '/heroes/slayer' }
         ]
+      },
+
+      {
+        text: 'Objetos',
+        collapsed: true,
+        items: [
+          { text: 'Todas las Rarezas', link: '/items/' }
+        ]
+      },
+
+      {
+        text: 'Guías',
+        collapsed: true,
+        items: [
+          { text: 'Principiantes', link: '/guides/beginners' },
+          { text: 'AFK Farm', link: '/guides/afk-farm' },
+          { text: 'Boss Guide', link: '/guides/boss-guide' }
+        ]
       }
-    ]
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/mikemizanin/TaskBarHero' }
+    ],
+
+    footer: {
+      message: 'TaskBarHero Wiki Español',
+      copyright: 'Proyecto de la comunidad'
+    }
   }
 })
