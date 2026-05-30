b6d344c234e16732b69d55cec8b818eda6b2d355
MikeMizanin <126438504+MikeMizanin@users.noreply.github.com>
2026-05-30 00:59:13 -0300

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,63 +6,19 @@ export default defineConfig({
 
   lang: 'es-ES',
 
-  lastUpdated: true,
-
   themeConfig: {
-    logo: '/logo.png',
-
     nav: [
       { text: 'Inicio', link: '/' },
-      { text: 'Héroes', link: '/heroes/knight' },
-      { text: 'Objetos', link: '/items/' },
-      { text: 'Guías', link: '/guides/' },
-      { text: 'Tier List', link: '/tier-list/' }
+      { text: 'Héroes', link: '/heroes/knight' }
     ],
-
-    search: {
-      provider: 'local'
-    },
 
     sidebar: [
       {
         text: 'Héroes',
-        collapsed: false,
         items: [
-          { text: 'Knight', link: '/heroes/knight' },
-          { text: 'Ranger', link: '/heroes/ranger' },
-          { text: 'Sorcerer', link: '/heroes/sorcerer' },
-          { text: 'Priest', link: '/heroes/priest' },
-          { text: 'Hunter', link: '/heroes/hunter' },
-          { text: 'Slayer', link: '/heroes/slayer' }
-        ]
-      },
-
-      {
-        text: 'Objetos',
-        collapsed: true,
-        items: [
-          { text: 'Todas las Rarezas', link: '/items/' }
-        ]
-      },
-
-      {
-        text: 'Guías',
-        collapsed: true,
-        items: [
-          { text: 'Principiantes', link: '/guides/beginners' },
-          { text: 'AFK Farm', link: '/guides/afk-farm' },
-          { text: 'Boss Guide', link: '/guides/boss-guide' }
+          { text: 'Knight', link: '/heroes/knight' }
         ]
       }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/mikemizanin/TaskBarHero' }
-    ],
-
-    footer: {
-      message: 'TaskBarHero Wiki Español',
-      copyright: 'Proyecto de la comunidad'
-    }
+    ]
   }
 })
