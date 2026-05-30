a17cdcc3bdc4330b15ed9f6b791fc35342c4ee7c
MikeMizanin <126438504+MikeMizanin@users.noreply.github.com>
2026-05-29 23:02:41 -0300

    config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,16 +1,16 @@
 export default {
   title: "Taskbar Hero Wiki",
-  description: "Base de datos en español",
+  description: "Base de datos en español", // <--- Acá agregamos la coma
   themeConfig: {
     nav: [
       { text: 'Inicio', link: '/' },
-      { text: 'Héroes', link: '/heroes/knight' }
+      { text: 'Héroes', link: '/heroes/knight' } // <--- Barra corregida
     ],
     sidebar: [
       {
         text: 'Guía de Héroes',
         items: [
-          { text: 'Knight (Caballero)', link: '/heroes/knight' },
+          { text: 'Knight (Caballero)', link: '/heroes/knight' }, // <--- Barras corregidas
           { text: 'Mage (Mago)', link: '/heroes/mage' }
         ]
       }
